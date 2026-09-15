6c90efd22ef762131cc96a26a332c414c9722b9c
suharti79-coder <suharti79@guru.smk.belajar.id>
2026-09-15 19:13:58 +0800

    Update print statement from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -665,7 +665,7 @@ elif st.session_state.role == "Admin":
                                     time.sleep(1)
                                     st.rerun()
 
-    st.markdown("### 3. Laporan & Rekap Harian Absensi")
+    st.markdown("### 📋 3. Laporan & Rekap Harian Absensi")
     
     with st.form("form_filter_rekap"):
         col_tgl, col_sch = st.columns(2)
